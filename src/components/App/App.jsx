8523bb1d7dc86cbe7e1b9925b564065bda019024
ali-zargari <77089778+ali-zargari@users.noreply.github.com>
2024-02-27 23:18:09 -0800

    minor bug fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -22,12 +22,37 @@ function App() {
 					content: chat.text
 				}));
 
-				const response = await axios.post("http://localhost:3001/api/message/GPT", { messages });
+				let response = "response_string";
+
+				if(algorithm === "GPT") {
+					response = await axios.post("http://localhost:3001/api/message/GPT", { messages });
+				}
+				else if(algorithm === "Gemini") {
+					response = await axios.post("http://localhost:3001/api/message/Gemini", { messages });
+				}
+				else if(algorithm === "Claude") {
+					response = await axios.post("http://localhost:3001/api/message/Claude", { messages });
+				}
+
+				//depending on the algorithm, the response will be different
+				//if the response is from GPT, the response will be in response.data.choices[0].message.content
+				let responseString = "response_string";
+
+				if(algorithm === "GPT") {
+					responseString = response.data.choices[0].message.content;
+
+				} else if(algorithm === "Gemini") {
+					//TODO: handle response from Gemini
+
+				}
 
 				const aiMessage = {
 					sender: "system",
-					text: response.data.choices[0].message.content
+					text: responseString
 				};
+
+
+
 
 				setChatHistory(prevChatHistory => [...prevChatHistory, aiMessage]);
 
@@ -48,9 +73,9 @@ function App() {
 			<h1>{algorithm}</h1> {/* Display the selected option */}
 
 			<select onChange={handleAlgorithmChange}>
-				<option value='Option 1'>Option 1</option>
-				<option value='Option 2'>Option 2</option>
-				<option value='Option 3'>Option 3</option>
+				<option value='GPT'>GPT</option>
+				<option value='Gemini'>Gemini</option>
+				<option value='Claude'>Claude</option>
 			</select>
 			<pre>
 				{
